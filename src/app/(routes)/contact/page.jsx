fb1a4187df6dc52f17e9b838ef32feac58a2c2ea
fzaidi96 <f.zaidi182@gmail.com>
2024-06-23 13:36:39 +0100

    add CTA styling

--- src/app/(routes)/contact/page.jsx
+++ src/app/(routes)/contact/page.jsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import styles from '@/app/page.module.css';
 import ContactBar from "@/components/ContactBar";
-import Image from "next/image";
-
 
 export default function ContactPage() {
     return (
@@ -14,19 +12,18 @@ export default function ContactPage() {
                 <Link href="/projects">Work</Link>
               </nav>
             </header>
-        
             <div className={styles.contactPage}>
                 <h1 className={styles.title}>Contact me .</h1>
                 <div className={styles.email}>
                 <p>Get in touch by dropping me an email:</p>
-                <div className={styles.emailBox}><button><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></button> <p>f.zaidi182@gmail.com</p></div>
+                <div className={styles.emailBox}><button><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></button> <p>f.zaidi182@gmail.com</p></div>
             </div>
             <div className={styles.socials}>
                 <p>Or catch me over on my socials:</p>
                 <div className={styles.contactBar}><ContactBar /></div>
             </div>
             <div className={styles.credits}>
-                    <Link href="#credits"><p><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 6H11L7.5 10.5L4 6Z" fill="currentColor"></path></svg></p></Link>
+                    <Link href="#credits"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 6H11L7.5 10.5L4 6Z" fill="currentColor"></path></svg></Link>
                 <div className={styles.credits} id="credits"><p>Grateful acknowledgment to the following for their invaluable resources, which made the creation of this portfolio possible:</p>
                 <ul>
                     <li>
--- src/app/(routes)/projects/page.jsx
+++ src/app/(routes)/projects/page.jsx
@@ -24,11 +24,12 @@ export default function ProjectsPage() {
                 <nav>
                     <ul className={styles.navList}>
                         <li><Link href="#section2">• Coding</Link></li>
-                        <li><Link href="#section3">• Socials</Link></li>
+                        <li><Link href="#section3">• Social Media</Link></li>
                         <li><Link href="#section4">• Copywriting</Link></li>
                         <li><Link href="#section5">• Branding & More</Link></li>
                     </ul>
                 </nav>
+                
             </div>
 
             {/* section two - coding */}
@@ -36,8 +37,7 @@ export default function ProjectsPage() {
               <container className={styles.section2}>     
              <div className={styles.codingProjects}>
                     <h1>web dev</h1> 
-                    <div className={styles.codingText}> <p>I&apos;m fresh out of a full-time 12-week bootcamp in full stack web development where I got hands-on with HTML, CSS, JS, SQL. Passionate about UI/UX, I love creating dynamic and visually stunning web apps. I have the ability to integrate third-party APIs, cloud storage and user authentication services into my builds. My projects demonstrate a range of skills including conditional rendering, game mechanics, data validation. My tech stack includes Node.js, Express.js, React, Next.js and Tailwind. Since completing the bootcamp, I&apos;ve been honing my skills with personal projects.
-                    Take a look through my projects to learn more!</p></div> 
+                    <div className={styles.codingText}> <p>I&apos;m fresh out of a full-time 12-week bootcamp in full stack web development where I got hands-on with HTML, CSS, JS, SQL. Passionate about UI/UX, I love creating dynamic and visually stunning web apps. I have the ability to integrate third-party APIs, cloud storage and user authentication services into my builds. My projects demonstrate a range of skills including conditional rendering, game mechanics, data validation. My tech stack includes Node.js, Express.js, React, Next.js and Tailwind. Since completing the bootcamp, I&apos;ve been honing my skills with personal projects.</p><p className={styles.CTA}><strong>Swipe through to check out my work!</strong></p></div> 
                 </div>
                 <Image src="/images/berlin.jpg" height={600} width={350} alt="image"/>
                 </container> 
@@ -46,7 +46,7 @@ export default function ProjectsPage() {
               {/* section three - social media content */}
             <div className={styles.socialContainer} id="section3"><div className={styles.socialProjects}>
                     <h1>social content</h1> 
-                    <div className={styles.socialText}> <p>I&apos;ve spent the last few years in marketing;  starting out on a grad scheme in PPC before diving into startup, where I found myself managing the entire online presence for a new social app, Bimble. I was behind the email marketing, PR, blog releases and more, but I truly found my groove in creating and managing social media content, especially short-form video (thank you TikTok)! Check out some of my favourite creations below!</p></div></div> 
+                    <div className={styles.socialText}> <p>I&apos;ve spent the last few years in marketing;  starting out on a grad scheme in PPC before diving into startup, where I found myself managing the entire online presence for a new social app, Bimble. I was behind the email marketing, PR, blog releases and more, but I truly found my groove in creating and managing social media content, especially short-form video (thank you TikTok)!</p> <p className={styles.CTA}><strong>Watch some of my favourite content here!</strong></p></div></div> 
                     <VideoGallery />
             </div>
 
@@ -60,7 +60,7 @@ export default function ProjectsPage() {
                     <div className={styles.img}><Link href="https://www.instagram.com/p/CamLsaYsVfQ/"><Image src="/images/four.png" width={200} height={200} alt="social media"/></Link></div>
                     <div className={styles.img}><Link href="https://bimbleapp.medium.com/this-christmas-gift-with-bimble-1c7e97143325"><Image src="/images/five.png" width={200} height={200} alt="social media"/></Link></div>
                 </div>
-                <p className={styles.copywritingText}>I have a degree in literature so naturally I know and love good writing. And through my experience in marketing, I have also come to know and love good copy. Some of my achiements include managing a blog - editing every piece and writing my own - with a readership of 20k, translating a German text for publication in English, and of course, writing 100s of social media captions. I&apos;ve selected some of my favourite posts below that represent my best work.</p>   
+                <p className={styles.copywritingTextCTA}><strong>I&apos;ve selected some of my favourite posts below that represent my best work.</strong></p> <p className={styles.copywritingText}>I have a degree in literature so naturally I know and love good writing. And through my experience in marketing, I have also come to know and love good copy. Some of my achiements include managing a blog - editing every piece and writing my own - with a readership of 20k, translating a German text for publication in English, and of course, writing 100s of social media captions.</p>    
                 <div className={styles.codingProjects}><h1>copywriting</h1></div>
              </container>
              </div>
@@ -73,26 +73,24 @@ export default function ProjectsPage() {
                     <h1>& more</h1></div> 
                 <div className={styles.miscContainer}>
                 
-                <div><div><Image src="/svg/evelyn.png" width={300} height={130} alt="evelyn logo" />
+                <div><div><Link href="https://www.evelynhealth.com/"><Image src="/svg/evelyn.png" width={300} height={130} alt="evelyn logo" /></Link>
                 <div className={styles.moreWorkText}>
                 <p className={styles.tagText}>• Crafted Evelyn&apos;s go-to-market strategy •</p>
                 <p className={styles.tagText}>• Delivered the brand identity •</p></div> 
                 </div></div>
                         
                         
-                        <div><div><Image src="/svg/bibak_logo.svg" width={400} height={130} alt="bibak logo" />
+                        <div><div><Link href="https://bi-bak.de/about-us"><Image src="/svg/bibak_logo.svg" width={400} height={130} alt="bibak logo" /></Link>
                         <div className={styles.moreWorkText}>
                         <p className={styles.tagText}>• Assisted on the launch of Sinema Transtopia •</p>
-                        <p className={styles.tagText}>• Translated a German release into English for publication •</p></div>
+                        <p className={styles.tagText}><Link href="https://bi-bak.de/en/publikationen/bitte-zurueckspulen-1">• Translated a German release into English for publication •</Link></p></div>
                         </div></div>
 
-                          <div><div><Image src="/svg/inchief.png" width={400} height={130} alt="inchief logo" />
+                          <div><div><Link href="https://inchief.com/our-work/"><Image src="/svg/inchief.png" width={400} height={130} alt="inchief logo" /></Link>
                           <div className={styles.moreWorkText}>
                             <p className={styles.tagText}>• Collaborated on a global influencer campaign •</p>
                             <p className={styles.tagText}>• Achieved 10.3m video views on TikTok in 12 weeks •</p></div>
                         </div></div>
-                          
-
                 </div>
 
 
